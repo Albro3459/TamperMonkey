392d4607848ab796e23a923016b5a4d3a439b703
Alex Brodsky <abrodsky@maruccisports.com>
2025-12-19 12:21:53 -0600

    Now the sidebar button toggles the sidebar :)

--- jsFiddleSidebar.js
+++ jsFiddleSidebar.js
@@ -8,57 +8,116 @@
 // @grant        none
 // ==/UserScript==
 
+let sidebarStyle$;
+
 (function () {
-  const css = `
-    /* this '@media' query is the fix. 'min-width: 0px' makes this always applied. Also // comments don't work in CSS */
-    @media (min-width: 0px) {
-        /* Inner css is exactly the same as the 'css/dist-editor.css?*' file */
-        body #layout-container header {
-            background-color:var(--sidebar-bg)
-        }
+    const el = document.querySelector('.sidebarToggle');
+    if (el) {
+        el.className = 'sidebarToggle'; // start clean
+    }
 
-        body #layout-container {
-            grid-template-columns: 55px 1fr;
-            grid-template-areas: "sidebar header" "content content"
-        }
+    function hideSidebar() {
+        const css = `
+            @media (min-width: 0px) {
+                body #layout-container header {
+                    background-color:var(--sidebar-bg)
+                }
 
-        body #fiddle-info {
-            left: 0
-        }
+                body #layout-container {
+                    grid-template-columns: 55px 1fr;
+                    grid-template-areas: "sidebar header" "content content"
+                }
 
-        body #fiddle-info .sidebarToggle {
-            display: none
-        }
+                body #fiddle-info {
+                    left: 0
+                }
 
-        body #sidebar {
-            grid-template-rows: 66px 0 0 0
-        }
+                body #sidebar {
+                    grid-template-rows: 66px 0 0 0
+                }
 
-        body #sidebar h1 svg {
-            width: 36px
-        }
+                body #sidebar h1 svg {
+                    width: 36px
+                }
 
-        body #sidebar #sidebar-footer,body #sidebar #sidebar-main,body #sidebar #sidebar-user,body .headerActions li.collaborateButton,body .headerActions li.divider,body .headerActions li.downloadButton,body .headerActions li.embedButton,body .headerActions li.favButton,body .headerActions li.gridButton,body .headerActions li.proButton,body .headerActions li.themeButton,body .headerActions li.togglePrivacy {
-            display: none
-        }
+                body #sidebar #sidebar-footer,body #sidebar #sidebar-main,body #sidebar #sidebar-user,body .headerActions li.collaborateButton,body .headerActions li.divider,body .headerActions li.downloadButton,body .headerActions li.embedButton,body .headerActions li.favButton,body .headerActions li.gridButton,body .headerActions li.proButton,body .headerActions li.themeButton,body .headerActions li.togglePrivacy {
+                    display: none
+                }
 
-        body .modalBody {
-            align-items: start
-        }
+                body .modalBody {
+                    align-items: start
+                }
 
-        body .modalApp {
-            min-width: auto;
-            height: auto;
-            min-height: auto
-        }
+                body .modalApp {
+                    min-width: auto;
+                    height: auto;
+                    min-height: auto
+                }
 
-        body #editor-options .fieldsCont {
-            grid-template-columns: 1fr 1fr
+                body #editor-options .fieldsCont {
+                    grid-template-columns: 1fr 1fr
+                }
+            }
+        `;
+
+        sidebarStyle$ = document.createElement("style");
+        sidebarStyle$.textContent = css;
+        document.documentElement.appendChild(sidebarStyle$);
+
+        // add disabled
+        const el = document.querySelector('.sidebarToggle');
+        if (el) {
+            el.className = 'sidebarToggle disabled';
         }
     }
-  `;
 
-  const style = document.createElement("style");
-  style.textContent = css;
-  document.documentElement.appendChild(style);
+    function showSidebar() {
+        if (sidebarStyle$) {
+            sidebarStyle$.remove();
+            sidebarStyle$ = null;
+        }
+
+        // remove disabled
+        const el = document.querySelector('.sidebarToggle');
+        if (el) {
+            el.className = 'sidebarToggle';
+        }
+    }
+
+    function toggleSidebar(show = false) {
+        if (!show) {
+            hideSidebar();
+        } else {
+            showSidebar();
+        }
+    }
+
+    function isSidebarDisabled() {
+        return document.querySelector('.sidebarToggle.disabled') !== null;
+    }
+
+    const sidebar = document.querySelector('[data-tippy-simple-content="Collapse sidebar"]');
+    if (!sidebar) {
+        // console.log("*** NO TOOLTIP ***");
+        toggleSidebar(false);
+    } else {
+        // Remove the (PRO) bubble on the tooltip:
+        sidebar.removeAttribute('data-tippy-pro');
+
+        document.addEventListener(
+            "click",
+            (e) => {
+                if (sidebar) {
+                    e.stopImmediatePropagation();
+                    e.preventDefault();
+
+                    toggleSidebar(isSidebarDisabled());
+                } else {
+                    // console.log("*** NO SIDEBAR TOGGLE ***");
+                }
+            },
+            true
+        );
+    }
+
 })();
